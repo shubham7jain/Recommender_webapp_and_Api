913acef4cc951c7b0f17933a1328b36a3100418f
Shubham Jain <shubham7jain@gmail.com>
2017-04-23 01:51:08 -0500

    Fixed mongo script

--- insert_mongo.py
+++ insert_mongo.py
@@ -4,10 +4,14 @@ import pymongo
 import json
 import json
 from collections import defaultdict
+from pymongo import Connection
+import time
 
-uri = 'mongodb://admin:password@ds115411.mlab.com:15411/meetup_data' 
-client = pymongo.MongoClient(uri)
-db = client.get_default_database()
+#uri = 'mongodb://admin:password@ds115411.mlab.com:15411/meetup_data' 
+#client = pymongo.MongoClient(uri)
+#db = client.get_default_database()
+connection = Connection()
+db = connection.meetup_data
 
 ### Adding Events Info Data
 events_json_file = open("data/LCHICAGO/events_info.json")
@@ -15,6 +19,8 @@ json_str = events_json_file.read()
 events_info = json.loads(json_str)
 
 db.events_info.remove()
+time.sleep(10)
+
 for event_id in events_info:
 	small_dict = {}
 	small_dict["event_id"] = str(event_id)
@@ -30,13 +36,14 @@ rsvp_json_file = open("data/LCHICAGO/rsvp_events.json")
 json_str = rsvp_json_file.read()
 event_members_map = json.loads(json_str)
 
+db.member_events.remove()
+time.sleep(10)
+
 member_events_map = defaultdict(lambda: [])
 for event in event_members_map:
 	member_ids = event_members_map[event]
 	for member in member_ids:
 		member_events_map[member].append(event)
-
-db.member_events.remove()
 
 for member_id in member_events_map:
 	small_dict = {}
