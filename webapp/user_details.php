187350a71b94c935b038a2133c809dd212628c24
Shubham Jain <shubham7jain@gmail.com>
2017-04-23 00:47:31 -0500

    Will do description thing later

--- webapp/user_details.php
+++ webapp/user_details.php
@@ -43,14 +43,6 @@
          
       </script>
       <script>
-      $(function(){ /* to make sure the script runs after page load */
-        $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
-            alert("Shubham");
-            event.preventDefault(); /* prevent the a from changing the url */
-            $(this).prev('#moretext').css('visibility', 'visible'); /* show the .more_text span */
-        });
-
-      });
         function myFunction() {
             document.getElementById("error").style.visibility = "hidden";
            var user_id = document.getElementById('styled').value
@@ -68,7 +60,7 @@
                    var i = 1;
                    for (var event in objes) {
                       rows += "<tr><td><b>" + i + "</b></td><td>" + event + "</td><td>" + 
-                      "<a href='#' class='read_more'>Read Description</a><br/><span style='visibility:hidden;' id='moretext'>" + objes[event].description + "</span></td><td>" + objes[event].lat + "</td><td>" + objes[event].lon + "</td><td>" + objes[event].time + "</td></tr>";
+                      + objes[event].description + "</td><td>" + objes[event].lat + "</td><td>" + objes[event].lon + "</td><td>" + objes[event].time + "</td></tr>";
                       i = i + 1;
                    }
                    rows += "</tbody></table>"
